6aa6cbffd6cedd0d7700d6c2603e9100f4f060fe
akasifislam <aseasifislam@gmail.com>
2023-08-26 02:24:10 +0600

    initial commit

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Company::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'address' => 'Test User',
+        ]);
+    }
+}
---
+++ resources/views/companies/action.blade.php
@@ -0,0 +1,9 @@
+<a href="{{ route('companies.edit', $id) }}" data-toggle="tooltip" data-original-title="Edit"
+    class="edit btn btn-success edit">
+    Edit
+</a>
+
+<a href="javascript:;" data-id="{{ $id }}" data-toggle="tooltip" data-original-title="Delete"
+    class="delete btn btn-danger">
+    Delete
+</a>
